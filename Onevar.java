94cbf67417331755e27416558569175ee82d3911
NishaAgar <nishakeny3@gmail.com>
2017-02-27 15:49:55 +0530

    Add files via upload

---
+++ Onevar.java
@@ -0,0 +1,15 @@
+
+public class Onevar {
+public static void main(String args[]){
+	int a=19;
+	if(a>0){
+		System.out.println("postive");
+	}
+	else if(a<0){
+		System.out.println("negative");
+	}
+	else{
+		System.out.println("zero");
+	}
+}
+}
