9b075bb7df707c0867c5f591ebed4bf06a3dd52f
NishaAgar <nishakeny3@gmail.com>
2017-02-28 10:14:43 +0530

    Add files via upload

---
+++ VowCons.java
@@ -0,0 +1,11 @@
+public class VowCons {
+	public static void main(String args[]) {
+		char a = 'u';
+		if (a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u') {
+			System.out.println("vowels");
+		} else {
+			System.out.println("constant");
+		}
+	}
+
+}
