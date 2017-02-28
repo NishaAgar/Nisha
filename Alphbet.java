c74523ce0267d324b6b6f7448ae3b55149553836
NishaAgar <nishakeny3@gmail.com>
2017-02-28 10:40:00 +0530

    Add files via upload

---
+++ Alphbet.java
@@ -0,0 +1,11 @@
+
+public class Alphbet {
+public static void main(String args[]){
+char c='v';
+if(Character.isLetter(c)){
+	System.out.println("alphbet");
+}
+else
+	System.out.println("not");
+}
+}
