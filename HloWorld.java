701358c65c3594f287c255acdbaca884db903a9a
NishaAgar <nishakeny3@gmail.com>
2017-02-28 11:21:32 +0530

    Add files via upload

--- HloWorld.java
+++ HloWorld.java
@@ -1,12 +1,10 @@
 
 public class HloWorld {
 public static void main(String args[]){
-	for(int i=0;i<5;i++){
-		System.out.println("Hello World");
-	}
-	/*String s="Hello World";
+	
+	String s="Hello World";
 	for(int i=0;i<5;i++){
 		System.out.println(s);
-	}*/
+	}
 }
 }
