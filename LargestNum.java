99fc9464c0e650fa7086de6432e8b976e17b5683
NishaAgar <nishakeny3@gmail.com>
2017-02-28 12:02:28 +0530

    Add files via upload

---
+++ LargestNum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class LargestNum {
+public static void main(String args[]){
+	int i=0;
+	float a[]=new float[3];
+	Scanner s=new Scanner(System.in);
+	for(i=0;i<a.length;i++){
+		a[i]=s.nextFloat();
+	if(a[0]<a[i]){
+	a[0]=a[i];
+	}
+		}
+	System.out.println("largest number"+a[0]);
+	}
+	
+		
+	
+		
+	}
+
+
