3aaa383d11083ae15b17369433d463e1e5b884dc
NishaAgar <nishakeny3@gmail.com>
2017-02-28 12:09:25 +0530

    Add files via upload

--- LargestNum.java
+++ LargestNum.java
@@ -1,23 +1,17 @@
 import java.util.Scanner;
 
-
 public class LargestNum {
-public static void main(String args[]){
-	int i=0;
-	float a[]=new float[3];
-	Scanner s=new Scanner(System.in);
-	for(;i<a.length;i++){
-		a[i]=s.nextFloat();
-	if(a[0]<a[i]){
-	a[0]=a[i];
-	}
+	public static void main(String args[]) {
+		int i = 0;
+		float a[] = new float[3];
+		Scanner s = new Scanner(System.in);
+		for (; i < a.length; i++) {
+			a[i] = s.nextFloat();
+			if (a[0] < a[i]) {
+				a[0] = a[i];
+			}
 		}
-	System.out.println("largest number"+a[0]);
-	}
-	
-		
-	
-		
+		System.out.println("largest number" + a[0]);
 	}
 
-
+}
