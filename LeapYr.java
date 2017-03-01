4d2ab80a800742e192ddd67ae90cd0bf46060589
NishaAgar <nishakeny3@gmail.com>
2017-03-01 10:09:21 +0530

    Add files via upload

---
+++ LeapYr.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class LeapYr {
+
+	public static void main(String[] args) {
+	
+		Scanner s = new Scanner(System.in);
+		int a = s.nextInt();
+		if((a%4==0 && a%100!=0)||a%400==0){
+			System.out.println("leap year"+" "+a);
+		}
+		else
+			System.out.println("not leap year");
+	}
+
+}
